0256b15191ada0e10aac570a6b317c2eebe80070
Eric Olson <eric@ericolsonconsulting.com>
2014-01-30 14:24:27 -0500

    Refactored recordset.php.

--- classes/recordset.php
+++ classes/recordset.php
@@ -1,25 +1,26 @@
 <?php
 
 class RecordSet {
-    public $query = '';
+    private $query = '';
+    private $results = array();
 
     public function setQuery($query_string){
         $this->query = $query_string;
     }
 
-    private function fetchRecords( $query ){
+    private function fetchRecords(){
         global $wpdb;
-        $results = $wpdb->get_results($query,ARRAY_A);
-        return $results;
+        $results = $wpdb->get_results($this->query,ARRAY_A);
+        $this->results = $results;
     }
 
     public function displayTable() {
-        $results = $this->fetchRecords($this->query);
-        if(count($results) == 0) {
+        $this->fetchRecords($this->query);
+        if(count($this->results) == 0) {
             echo '<em>No rows returned</em>';
         } else {
-                echo '<table><thead><tr><th class="eocdbr">'.implode('</th><th class="eocdbr">', array_keys(reset($results))).'</th></tr></thead><tbody>'."\n";
-                foreach($results as $result) {
+                echo '<table><thead><tr><th class="eocdbr">'.implode('</th><th class="eocdbr">', array_keys(reset($this->results))).'</th></tr></thead><tbody>'."\n";
+                foreach($this->results as $result) {
                     echo '<tr><td>'.implode('</td><td>', array_values($result)).'</td></tr>'."\n";
                 }
                 echo '</tbody></table>';
