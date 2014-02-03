1a7b1dda7e47fa2793071d2f6e47966af472f64a
Eric Olson <eric@ericolsonconsulting.com>
2014-02-03 15:17:00 -0500

    DDL for wp_dbr_queries table.

---
+++ sql/wp_dbr_queries.sql
@@ -0,0 +1,5 @@
+CREATE TABLE wp_dbr_queries (
+    query_id int(11) NOT NULL AUTO_INCREMENT,
+    query text,
+    PRIMARY KEY (query_id)
+    ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1
